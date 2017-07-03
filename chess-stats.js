7e340966f17866c3923fcd89730a20fd7d12d08e
Kevin Tamarus Tang <kevintamarus@gmail.com>
2017-07-03 08:18:05 -0700

    fixed errors

--- chess-stats.js
+++ chess-stats.js
@@ -1,5 +1,5 @@
 var Chess = function() {
-  var games = {
+  this.games = {
     1 : {date:"june25", white:'Eddie', black:'Griff', winner:'Eddie', loser:'Griff'},
     2 : {date:"june26", white:'Griff', black:'Eddie', winner:'Griff', loser:'Eddie'},
     3 : {date:"june26", white:'Kevin', black:'Alex', winner:'Kevin',loser:'Alex'},
@@ -7,9 +7,9 @@ var Chess = function() {
     5 : {date:"june27", white:'Alex', black:'Eddie', winner:'Alex',loser:'Eddie'},
     6 : {date:"june28", white:'Eddie', black:'Griff', winner:'Eddie',loser:'Griff'},
     7 : {date:"june29", white:'Eddie', black:'Griff', winner:'Griff',loser:'Eddie'},
-    total : 7
   };
-  var ratio = {
+  this.totalGames = 7;
+  this.stats = {
     Alex : {wins: 2, losses: 1, draws: 0},
     Eddie : {wins: 2, losses: 3, draws: 0},
     Griff : {wins: 2, losses: 2, draws: 0},
@@ -24,60 +24,65 @@ var Match = function(date, playerWhite, playerBlack, winner, loser) {
   match.white = playerWhite;
   match.black = playerBlack;
   match.winner = winner;
-  match.loser = lower;
+  match.loser = loser;
 }; 
 
 //add games or players
 Chess.prototype.addGame = function(date, playerWhite, playerBlack, winner, loser) {
   var matchInsert = new Match(date, playerWhite, playerBlack, winner, loser);
-  this.games.total++;
-  this.games[total] = matchInsert;
+  this.totalGames++;
+  this.games[this.totalGames] = matchInsert;
   if (winner === 'draw' || winner === 'Draw') {
-    this.ratio[playerWhite].draws++;
-    this.ratio[playerBlack].draws++;
+    this.stats[playerWhite].draws++;
+    this.stats[playerBlack].draws++;
   }
   else {
-    this.ratio[winner].wins++;
-    this.ratio[loser].losses++;
+    this.stats[winner].wins++;
+    this.stats[loser].losses++;
   }
 };
 
 Chess.prototype.addPlayer = function(player) {
-  this.ratio[player] = {wins: 0, losses: 0, draws: 0};
+  this.stats[player] = {wins: 0, losses: 0, draws: 0};
 }
 
 //info/stats retrival
-Chess.prototype.winRatio = function(player) {
-  var wins = this.ratio[player].wins;
-  var losses = this.ratio[player].losses;
+Chess.prototype.winPercentage = function(player) {
+  var wins = this.stats[player].wins;
+  var losses = this.stats[player].losses;
   return player + " has a " + ((wins/(wins + losses))*100).toString() + '% win rate';
 };
 
 Chess.prototype.totalGames = function() {
-  return this.games.total;
+  return this.totalGames;
 }
 
 Chess.prototype.searchGameNumber = function(number) {
   return this.games[number];
 }
 
+Chess.prototype.searchPlayerStats = function(player) {
+  return this.stats;
+}
+
 Chess.prototype.searchPlayerHistory = function(player) {
   var array = [];
   for(var match in this.games) {
-    if(typeof Match === "number") {
-      if (match.white === player) {
-        array.push(match);
-      }
-      if (match.white === player) {
-        array.push(match);
-      }
+    if (this.games[match].white === player) {
+      array.push(this.games[match]);
+    }
+    if (this.games[match].black === player) {
+      array.push(this.games[match]);
     }
   }
   return array;
 };
 
-console.log(Chess);
-console.log(Chess.winRatio("Alex"));
-console.log(Chess.winRatio("Eddie"));
-console.log(Chess.winRatio("Griff"));
-console.log(Chess.winRatio("Kevin"));
+var chess = new Chess;
+
+// console.log(chess.winPercentage("Alex"));
+// console.log(chess.winPercentage("Eddie"));
+// console.log(chess.winPercentage("Griff"));
+// console.log(chess.winPercentage("Kevin"));
+
+console.log(chess.searchPlayerHistory("Kevin"));
